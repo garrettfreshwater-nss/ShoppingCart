9f77f4a544683d83f868c4385f9638c1e9550391
garrettfreshwater-nss <gfresh.nss@gmail.com>
2020-04-21 16:13:59 -0400

    User can register and login/

---
+++ ShoppingCart/Models/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace ShoppingCart.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
